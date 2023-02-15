469f1fb193b7f4d55053bb55a13b9032010e55b4
szerr <szerr@sina.cn>
2023-02-16 06:38:29 +0800

    crond 中 run-parts 命令无法调用 python 脚本，暂且写个脚本绕过

---
+++ src/drops/docker_ops/servers/drops/bin/drops-backup.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+
+import os
+
+
+class CmdRunError(Exception):
+    def __str__(self):
+        return "命令执行错误: "+self.args[0]
+
+
+backupBin = 'drops backup -c -d %Y-%m-%d -t /srv/backup  -f'
+
+os.chdir('/srv/drops/')
+for p in os.listdir('.'):
+    if os.path.isdir(p):
+        print('-------------', p, '--------------')
+        os.chdir(p)
+        s = os.system(backupBin)
+        if s != 0:
+            raise CmdRunError(backupBin)
+        os.chdir('..')
--- src/drops/docker_ops/servers/drops/cron_example/README.md
+++ src/drops/docker_ops/servers/drops/cron_example/README.md
@@ -1,3 +1,5 @@
 # periodic_example
 
 在定时任务里常用的脚本示例
+注意，即使 run-parts 可以执行其他脚本，在 crond 中只能执行 shell 脚本，其他不会被执行也没有错误信息和日志。
+要执行 drops-backup.py ，用 drops-backup 调用
